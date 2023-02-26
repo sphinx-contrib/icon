df6042818d69051da394af70f83371137f4da108
Pierrick Rambaud <pierrick.rambaud49@gmail.com>
2023-02-26 22:30:23 +0100

    fix: use fonts directly from the npm package

--- sphinxcontrib/icon/__init__.py
+++ sphinxcontrib/icon/__init__.py
@@ -14,10 +14,22 @@ __email__ = "pierrick.rambaud49@gmail.com"
 
 def setup(app: Sphinx) -> Dict[str, Any]:
     """Add icon node to the sphinx builder."""
+    # load the icon node/role
     app.add_node(icon_node, **_NODE_VISITORS)  # type: ignore
     app.add_role("icon", Icon())
-    app.add_css_file(str(Fontawesome.css_file.resolve()))
-    app.add_js_file(str(Fontawesome.js_file.resolve()))
+
+    # load the font
+    font_handler = Fontawesome()
+
+    # install html related files
+    app.add_css_file(str(font_handler.css_file.resolve()))
+    app.add_js_file(str(font_handler.js_file.resolve()))
+
+    # install latex files
+    app.add_latex_package("fontspec")
+    app.connect("config-inited", font_handler.add_latex_font)
+    app.connect("config-inited", font_handler.enforce_xelatex)
+    app.connect("builder-inited", font_handler.add_latex_font_files)
 
     return {
         "version": __version__,

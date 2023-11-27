572be50c394d968620d9bf4ba2ba464fab819fd5
Rambaud Pierrick <12rambau@users.noreply.github.com>
2023-11-27 18:35:00 +0100

    fix: use pathlib to be windows compatible

--- noxfile.py
+++ noxfile.py
@@ -2,6 +2,7 @@
 
 The nox run are build in isolated environment that will be stored in .nox. to force the venv update, remove the .nox/xxx folder.
 """
+from pathlib import Path
 
 import nox
 
@@ -11,7 +12,8 @@ def docs(session):
     """Build the documentation."""
     session.install(".[doc]")
     b = session.posargs[0] if session.posargs else "html"
-    session.run("sphinx-build", f"-b={b}", "-a", "-E", "docs", f"docs/_build/{b}")
+    dst = Path(__filename__)/"docs"/"_builds"/b
+    session.run("sphinx-build", f"-b={b}", "-a", "-E", "docs", str(dst))
 
 
 @nox.session(reuse_venv=True)

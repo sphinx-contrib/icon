dcc126aa8d13afd78e64c1c0d7e4f6e724ba52ce
Rambaud Pierrick <12rambau@users.noreply.github.com>
2024-01-08 08:08:18 +0000

    fix: keep HERE relative

--- sphinxcontrib/icon/font_handler.py
+++ sphinxcontrib/icon/font_handler.py
@@ -9,7 +9,7 @@ from sphinx.util import logging
 from sphinx.util.osutil import copyfile, ensuredir
 from yaml import safe_load
 
-HERE = Path(__file__).parent.resolve()
+HERE = Path(__file__).parent
 
 logger = logging.getLogger(__name__)
 
